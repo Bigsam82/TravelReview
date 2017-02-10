7468eb38545717cf59342c3b929cfba6f99907e3
Samuel Saunders <sam.saunders116@yahoo.com>
2017-02-10 08:12:48 -0500

    initial

---
+++ TravelReview1/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TravelReview1.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+        public string Travel_Category { get; set; }
+        public DateTime Date { get; set; }
+        public string Purpose { get; set; }
+
+        public virtual ICollection<Review> Review { get; set; }
+
+    }
+}
